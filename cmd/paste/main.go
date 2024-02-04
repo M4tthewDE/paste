b690d709bbe307a7ffbeae34c76d969d3e595011
M4tthewDE <matthias.kronberg@gmail.com>
2024-02-04 18:54:01 +0100

    Switch to gin

--- cmd/paste/main.go
+++ cmd/paste/main.go
@@ -5,11 +5,10 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/a-h/templ"
 	"github.com/alecthomas/chroma/formatters/html"
 	"github.com/alecthomas/chroma/lexers"
 	"github.com/alecthomas/chroma/styles"
-	"github.com/go-chi/chi/v5"
+	"github.com/gin-gonic/gin"
 	"github.com/m4tthewde/paste/internal"
 )
 
@@ -25,111 +24,85 @@ func main() {
 
 	config = c
 
-	r := chi.NewRouter()
-	r.Handle("/", templ.Handler(internal.Index()))
-	r.Post("/upload/paste", uploadHandler)
-	r.Get("/{slug}", slugHandler)
-	r.Get("/info/health", healthHandler)
-
-	log.Println("Listening on :" + config.Port)
-	http.ListenAndServe(":"+config.Port, r)
-}
-
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	content := r.FormValue("content")
-	if content == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	slug := internal.Slug(config.SlugLength)
-
-	err = internal.Upload(r.Context(), config.BucketName, slug, content)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	_, err = w.Write([]byte(slug))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-}
-
-func slugHandler(w http.ResponseWriter, r *http.Request) {
-	slug := chi.URLParam(r, "slug")
-	if slug == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	c, err := internal.Download(r.Context(), config.BucketName, slug)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-
-	if r.URL.Query().Has("raw") {
-		_, err := w.Write([]byte(c))
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		err := internal.Index().Render(c.Request.Context(), c.Writer)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			c.String(http.StatusInternalServerError, "")
+		}
+	})
+
+	r.POST("/upload/paste", func(c *gin.Context) {
+
+		content := c.PostForm("content")
+		if content == "" {
+			c.String(http.StatusBadRequest, "")
+			return
 		}
 
-		return
-	}
+		slug := internal.Slug(config.SlugLength)
 
-	content := string(c)
+		err = internal.Upload(c.Request.Context(), config.BucketName, slug, content)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "")
+			return
+		}
 
-	lexer := lexers.Analyse(content)
-	if lexer == nil {
-		lexer = lexers.Fallback
-	}
+		c.String(http.StatusOK, slug)
+	})
 
-	iterator, err := lexer.Tokenise(nil, content)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	r.GET("/:slug", func(c *gin.Context) {
+		slug := c.Param("slug")
+		if slug == "" {
+			c.String(http.StatusBadRequest, "")
+			return
+		}
 
-	style := styles.Get("github")
-	if style == nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		contentBytes, err := internal.Download(c.Request.Context(), config.BucketName, slug)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "")
+		}
 
-	formatter := html.New(html.Standalone(false), html.WithLineNumbers(true))
-	if formatter == nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		content := string(contentBytes)
 
-	var paste bytes.Buffer
+		lexer := lexers.Analyse(content)
+		if lexer == nil {
+			lexer = lexers.Fallback
+		}
 
-	err = formatter.Format(&paste, style, iterator)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		iterator, err := lexer.Tokenise(nil, content)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "")
+			return
+		}
 
-	component := internal.Paste(paste.String())
+		style := styles.Get("github")
+		if style == nil {
+			c.String(http.StatusInternalServerError, "")
+			return
+		}
 
-	err = component.Render(r.Context(), w)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-}
+		formatter := html.New(html.Standalone(false), html.WithLineNumbers(true))
+		if formatter == nil {
+			c.String(http.StatusInternalServerError, "")
+			return
+		}
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("UP"))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		var paste bytes.Buffer
+
+		err = formatter.Format(&paste, style, iterator)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "")
+			return
+		}
+
+		err = internal.Paste(paste.String()).Render(c.Request.Context(), c.Writer)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "")
+			return
+		}
+	})
+
+	log.Println("Listening on :" + config.Port)
+	r.Run(":" + config.Port)
 }
